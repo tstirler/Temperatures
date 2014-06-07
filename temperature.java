6b2e5dbff0caed51f80b09e970003b4c8c289b7f
tstirler <torbjorn@stirler.no>
2014-06-07 12:31:55 +0200

    functioning C to  F

--- temperature.java
+++ temperature.java
@@ -1,48 +1,79 @@
 import java.util.Scanner;
 public class temperature {
   public static void main(String[] args) {
+    clearConsole();
+    splashScreen();
+    String scale = selectScale();
+
+    selectTemperature(scale);
   }
 
-  public static double celToFar(celTemp) {
-    return celtemp;
+  public static double celToFar(double celTemp) {
+    double newTemp = ((celTemp * 9) / 5) + 32;
+    return newTemp;
   }
 
-  public static double farToCel(farTemp) {
-    return farTemp;
+  public static double farToCel(double farTemp) {
+    double newTemp = (farTemp-32) * (5 / 9);
+    return newTemp;
   }
 
-  public static void splashScreen {
+  public static void splashScreen() {
     System.out.println("");
-    System.out.println(" ___________________________________________________ ");
-    System.out.println("|                                                   |");
-    System.out.println("|            Simple temperature converter           |");
-    System.out.println("|___________________________________________________|");
+    System.out.println("   ___________________________________________________ ");
+    System.out.println("  |                                                   |");
+    System.out.println("  |            Simple temperature converter           |");
+    System.out.println("  |___________________________________________________|");
     System.out.println("");
     System.out.println("");
   }
-}
 
+  public static String getString() {
+    System.out.print(" > "); // Add a nice prompt.
+    Scanner in = new Scanner(System.in);
+    String userInput = in.nextLine();
+    userInput = userInput.toLowerCase();
+    return userInput;
+  }
 
+  public static double getNumber() {
+    System.out.print(" > "); // Add a nice prompt.
+    Scanner in = new Scanner(System.in);
+    int userNumberInput = in.nextInt();
+    return userNumberInput;
+  }
 
+  public static String selectScale() {
+    System.out.println("Do you wish to convert from (C)elcius to Farenheit, or from (F)arenheit to Celcius?");
+    String userScaleInput = getString();
+    if (userScaleInput.equals("f")) {
+      System.out.println("You've selected Farenheit.");
+      return userScaleInput;
+    } else if (userScaleInput.equals("c")) {
+      System.out.println("You've selected Celcius.");
+      return userScaleInput;
+    } else {
+      System.out.println("You have not selected a usable temperature scale.");
+    }
+    return "not selected";
+  }
 
-var temperature = 100;
-var tempscale = "f";
+  public static void selectTemperature(String scale) {
+    System.out.println("What temperature would you like to convert?");
 
-var celToFar = function(oldTemp) {  //Define the Celcius to Farenheit function
-    var newTemperature = (oldTemp-32)*(5/9); //Convert the temperatur
-    return newTemperature.toPrecision(4); //Push back the new temperatur
-};
+    double userTemp = getNumber();
+    if (scale.equals("f")) {
+      double newTemp = farToCel(userTemp);
+      System.out.println("");
+      System.out.println(userTemp + " in Farenheit is " + newTemp);
+    } else if (scale.equals("c")) {
+      double newTemp = celToFar(userTemp);
+      System.out.println("");
+      System.out.println(userTemp + " in Celcius is " + newTemp);
+    }
+  }
 
-var farToCel = function(oldTemp) {  //Define the Farenheit to Celcius function
-    var newTemperature = ((oldTemp*9)/5)+32; //Convert the temperatur
-    return newTemperature.toPrecision(4); //Push back the new temperatur
-};
-
-
-if (tempscale.toLowerCase() === "c") { // Check to see if you are converting Celcius
-    console.log(temperature + " in Celcius is " + celToFar(temperature) + " in Farenheit.");
-} else if (tempscale.toLowerCase() === "f") { // Check to see if you are converting Farenheit
-    console.log(temperature + " in Farenheit is " + farToCel(temperature) + " in Celcius.");
-} else {
-    console.log("Looks like you did not select a correct Scale.")
+  public static void clearConsole() {
+    for (int i = 0; i < 80; ++i) System.out.println();
+  }
 }
